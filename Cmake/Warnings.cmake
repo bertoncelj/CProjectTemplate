a9c77690a85b3cbd9a1ec34dbf171c232dffbe63
tine <tine@gmail.com>
2023-10-10 18:17:28 +0200

    init

---
+++ Cmake/Warnings.cmake
@@ -0,0 +1,47 @@
+function(target_set_warnings)
+  set(oneValueArgs TARGET ENABLE AS_ERRORS)
+  cmake_parse_arguments(
+        TARGET_SET_WARNINGS
+        "${options}"
+        "${oneValueArgs}"
+        "${multiValueArgs}"
+        ${ARGN})
+
+  if(NOT ${TARGET_SET_WARNINGS_ENABLE})
+      message(STATUS "Warnings Disabled for: ${TARGET_SET_WARNINGS_TARGET}")
+      return()
+  endif()
+
+        # NOTE: add for MSVC
+
+  # extented for gcc
+  set(CLANG_WARNINGS
+      # Baseline
+      -Wall
+      -Wextra # reasonable and standard
+      -Wshadow # if a variable declaration shadows one from a parent context
+      -Wpedantic # warn if non-standard is used
+      # C and C++ Warnings
+      -Wunused # warn on anything being unused
+      -Wformat=2 # warn on security issues around functions that format output
+      -Wcast-align # warn for potential performance problem casts
+      -Wconversion # warn on type conversions that may lose data
+      -Wsign-conversion # warn on sign conversions
+      -Wnull-dereference # warn if a null dereference is detected
+      -Wdouble-promotion # warn if float is implicit promoted to double
+  )
+
+  set(GCC_WARNINGS
+      ${CLANG_WARNINGS}
+      -Wduplicated-cond # warn if if / else chain has duplicated conditions
+      # -Wduplicated-branches # warn if if / else branches have duplicated code
+      -Wlogical-op # warn about logical operations being used where bitwise were probably wanted
+  )
+
+  if(${TARGET_SET_WARNINGS_AS_ERRORS})
+    set(GCC_WARNINGS ${GCC_WARNINGS} -Werror)
+  endif()
+
+  target_compile_options(${TARGET_SET_WARNINGS_TARGET} PUBLIC ${GCC_WARNINGS})
+
+endfunction(target_set_warnings)
